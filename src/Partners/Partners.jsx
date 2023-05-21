2fc18ef928dc618942309ea044ae54c2401e9d42
AhmedRahi <rezarahi71@gmail.com>
2023-05-21 19:33:46 +0600

    retail partner 1image  added

--- src/Partners/Partners.jsx
+++ src/Partners/Partners.jsx
@@ -15,13 +15,13 @@ const Partners = () => {
                 <h2 className="text-4xl py-10 font-bold text-black text-center">
                     Our Retail Partners
                 </h2>
-                {/* <div data-aos="fade-up" className="grid grid-cols-2 md:grid-cols-4 gap-4 
+                <div data-aos="fade-up" className="grid grid-cols-2 md:grid-cols-4 gap-4 
                 ms-10 md:ms-20">
                     <img className="rounded" src="https://i0.wp.com/abctoysbd.com/wp-content/uploads/2023/05/unimart.jpg?resize=150%2C150&ssl=1" alt="" />
-                    <img className="w-40 rounded" src="https://i0.wp.com/abctoysbd.com/wp-content/uploads/2023/05/kids-mom.jpg?resize=150%2C150&ssl=1" alt="" />
+                    {/* <img className="w-40 rounded" src="https://i0.wp.com/abctoysbd.com/wp-content/uploads/2023/05/kids-mom.jpg?resize=150%2C150&ssl=1" alt="" />
                     <img className="rounded" src="https://i0.wp.com/abctoysbd.com/wp-content/uploads/2023/05/well-being.jpg?resize=150%2C150&ssl=1" alt="" />
-                    <img className="w-40 rounded" src="https://i0.wp.com/abctoysbd.com/wp-content/uploads/2023/05/lavander.jpg?resize=150%2C150&ssl=1" alt="" />
-                </div> */}
+                    <img className="w-40 rounded" src="https://i0.wp.com/abctoysbd.com/wp-content/uploads/2023/05/lavander.jpg?resize=150%2C150&ssl=1" alt="" /> */}
+                </div>
             </div>
             <div>
                 {/* <h2 className="text-4xl mt-20 mb-10 font-bold text-black text-center">
